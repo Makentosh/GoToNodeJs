e343b599b58fb9bda76673b2880ebd522c0eb84a
Makentosh <ziki_007@ukr.net>
2020-10-27 23:19:10 +0200

    add info comment

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 
+
+//configuration handlebars
 const hbs = exphbs.create({
   defaultLayout: 'main',
   extname: 'hbs'
 })
 
+//register express engine from handlebars
 app.engine('hbs', hbs.engine)
+//registration handlebars to express from name = hbs
 app.set('view engine', 'hbs')
+//set default views folder
 app.set('views', 'views')
-
+//set express statis folder to css & js files
 app.use(express.static('public'))
+
+
+
+//routes to page
 
 app.get('/', (req, res) => {
   res.render('index')
@@ -23,7 +32,7 @@ app.get('/about', (req, res) => {
 
 const PORT = process.env.PORT || 3000
 
-
+//start express server
 app.listen(PORT, () => {
-  console.log(`server run PORT ${PORT}`)
+
 })
